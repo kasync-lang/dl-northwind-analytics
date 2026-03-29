de1c3d27f5dd3ac01f67790b7c40807bad0b0ba1
kasync-lang <nguyenkhoir2@gmail.com>
2026-03-30 05:13:58 +0700

    Created dimension date warehouse layer

--- northwind/models/warehouse/fact_sales.sql
+++ northwind/models/warehouse/fact_sales.sql
@@ -1,9 +1,30 @@
 with source as (
-    select * from {{ ref('stg_orders') }} o
+    select
+        od.order_id,
+        od.product_id,
+        od.quantity,
+        od.unit_price,
+        od.discount,
+        od.status_id,
+        od.date_allocated,
+        od.purchase_order_id,
+        od.inventory_id,
+        o.customer_id,
+        o.employee_id,
+        o.shipper_id,
+        o.order_date,
+        o.shipped_date,
+        o.paid_date,
+        current_timestamp() as insertion_timestamp,
+        from {{ ref('stg_orders') }} o
     left join {{ ref('stg_order_details') }} od
-    on o.order_id = od.id
+    on o.id = od.order_id
     where od.order_id is not null
 ),
 unique_source as (
-    select *, row_number() over(partition by customer_id, employee_id, order_id, product_id, shipper_id, purchase_order_id, shipper) as row_number from source
+    select *, row_number() over(partition by customer_id, employee_id, order_id, product_id, shipper_id, purchase_order_id, shipper_id, order_date) as row_number from source
 )
+select *
+except (row_number)
+from unique_source
+where row_number = 1
